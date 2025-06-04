2faffc1dd0772b64863170ac506c113c458a5a66
Ravivarma Gollapalli <156598466+Varma0214@users.noreply.github.com>
2025-06-04 22:13:46 +0530

    Update AdminSignup.js

--- client/src/components/AdminSignup.js
+++ client/src/components/AdminSignup.js
@@ -12,7 +12,7 @@ function AdminSignup() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/register', {
+      const res = await axios.post('https://blogging-platform-backend-cp1y.onrender.com/api/auth/register', {
         email,
         password,
       });
