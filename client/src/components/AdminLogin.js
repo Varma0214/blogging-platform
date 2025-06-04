973f2c51572a9c051754f48375d74aaff1d80cd4
Ravivarma Gollapalli <156598466+Varma0214@users.noreply.github.com>
2025-06-04 22:13:23 +0530

    Update AdminLogin.js

--- client/src/components/AdminLogin.js
+++ client/src/components/AdminLogin.js
@@ -12,7 +12,7 @@ function AdminLogin() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/login', {
+      const res = await axios.post('https://blogging-platform-backend-cp1y.onrender.com/api/auth/login', {
         email,
         password,
       });
